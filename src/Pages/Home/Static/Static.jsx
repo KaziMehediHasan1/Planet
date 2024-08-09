0a964a4c5ce0951bd090cfad4cb2482a561d0e41
kazimehedihasan1 <mehedihasanarabi00@gmail.com>
2024-08-09 20:15:34 +0600

    set premium user in static chart

--- src/Pages/Home/Static/Static.jsx
+++ src/Pages/Home/Static/Static.jsx
@@ -2,29 +2,38 @@ import { useEffect, useState } from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import useAllUsers from "../../../hooks/useAllUsers/useAllUsers";
+import usePayment from "../../../hooks/Payment/usePayment";
 
 const Static = () => {
-  const [users, refetch, isLoading, error] = useAllUsers();
-  const [counts, setCounts] = useState({
-    allUsers: 0,
-    normalUsers: 0,
-    premiumUsers: 0,
-  });
-  // console.log(users.length);
-
+  const [allUser, setAllUsers] = useState(0);
+  const [normalUser, setNormalUser] = useState(0);
+  const [premiumUser, setPremiumUser] = useState(0);
+  const [users] = useAllUsers();
+  const [payment] = usePayment();
+  console.log(payment);
   useEffect(() => {
-    if (users) {
-      const total = users.length;
-      const normal = users.filter(user => !user.isPremium).length;
-      const premium = users.filter(user => user.isPremium).length;
-
-      setCounts({
-        allUsers: total,
-        normalUsers: normal,
-        premiumUsers: premium,
-      });
-    }
-  }, [users]);
+    setTimeout(() => {
+      if (allUser < users.length) {
+        setAllUsers(allUser + 1);
+      }
+    }, 10);
+  }, [allUser]);
+  // normal user..
+  useEffect(() => {
+    setTimeout(() => {
+      if (allUser < users.length) {
+        setAllUsers(allUser + 1);
+      }
+    }, 10);
+  }, [allUser]);
+  // premium user...
+  useEffect(() => {
+    setTimeout(() => {
+      if (premiumUser < payment.length) {
+        setPremiumUser(allUser + 1);
+      }
+    }, 10);
+  }, [allUser]);
 
   return (
     <div className="border-t-2 rounded-t-xl mt-10 bg-[#f7f5f6] border-blue-300">
@@ -36,32 +45,32 @@ const Static = () => {
 
           <CircularProgressbar
             className="drop-shadow-lg"
-            value={counts}
-            text={`${counts.allUsers}%`}
+            value={allUser}
+            text={`${allUser}`}
           ></CircularProgressbar>
         </div>
         <div></div>
-        {/* <div className="text-center space-y-5">
+        <div className="text-center space-y-5">
           <label className="lg:text-2xl sm:text-xs font-medium inline-block">
             Normal Users
           </label>
           <CircularProgressbar
             className="drop-shadow-lg"
-            value={percentage}
-            text={`${percentage}%`}
+            value={allUser}
+            text={`${allUser}`}
           />
-        </div> */}
+        </div>
         <div></div>
-        {/* <div className="text-center space-y-5">
+        <div className="text-center space-y-5">
           <label className="lg:text-2xl sm:text-xs font-medium inline-block">
             Premium Users
           </label>
           <CircularProgressbar
             className="drop-shadow-lg"
-            value={percentage}
-            text={`${percentage}%`}
+            value={allUser}
+            text={`${allUser}`}
           />
-        </div> */}
+        </div>
       </div>
     </div>
   );
